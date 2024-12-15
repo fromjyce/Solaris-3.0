9df1c651cf7ecc100e994b3f36758b4b7adba3e2
Jayashre <jaya2004kra@gmail.com>
2024-12-15 09:45:34 +0530

    added footer

---
+++ frontend/components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <div>
+      <footer className="bg-[#072000ff] text-white py-1">
+        <div className="container mx-auto text-center kumbhSans">
+         Developed by <strong>Team Simpsons</strong> for <strong>I ❤️ Hackathon</strong>
+        </div>
+      </footer>
+    </div>
+  );
+}
