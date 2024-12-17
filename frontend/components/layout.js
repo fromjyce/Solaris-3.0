fda08fce038de85bdf3aeb8a6c23dc71e0d49ad7
Jayashre <jaya2004kra@gmail.com>
2024-12-17 23:16:11 +0530

    added layout

---
+++ frontend/components/layout.js
@@ -0,0 +1,14 @@
+import Sidebar from "./sidebar";
+
+const Layout = ({ children }) => {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <main className="flex-grow  bg-gray-100 min-h-screen overflow-y-auto ml-64">
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
