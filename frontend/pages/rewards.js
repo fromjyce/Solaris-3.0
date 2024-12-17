c07b7f9831c1733d78729275fe092a2626a9acb6
Jayashre <jaya2004kra@gmail.com>
2024-12-18 00:59:43 +0530

    added rewards page

---
+++ frontend/pages/rewards.js
@@ -0,0 +1,26 @@
+import RewardsOverview from "@/components/rewards/RewardsOverview";
+import RewardsActions from "@/components/rewards/RewardsActions";
+import Leaderboard from "@/components/rewards/Leaderboard";
+import Layout from "@/components/layout";
+import Head from "next/head";
+
+export default function Rewards() {
+  return (
+    <>
+    <Head>
+        <title>Solaris 2.0 | Rewards</title>
+    </Head>
+    <div className="flex min-h-screen bg-[#cfcbbdff]">
+    <Layout />
+    <div className="container mx-auto p-8">
+      <h1 className="text-4xl font-bold mb-6 kumbhSans text-[#072000ff]">Rewards</h1>
+      <main className="mx-auto space-y-8">
+        <RewardsOverview />
+        <RewardsActions />
+        <Leaderboard />
+      </main>
+    </div>
+    </div>
+    </>
+  );
+}
