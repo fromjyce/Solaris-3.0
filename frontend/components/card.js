18086ce2786d9f3a2c3fe15806c90bc683efb9e8
Jayashre <jaya2004kra@gmail.com>
2024-12-17 23:45:31 +0530

    changed the font

--- frontend/components/card.js
+++ frontend/components/card.js
@@ -1,7 +1,7 @@
 export default function Card({ title, value }) {
     return (
       <div className="bg-[#f4f4f4] shadow-md rounded-lg p-4 gabarito">
-        <h2 className="text-[#072000ff] text-sm font-medium">{title}</h2>
+        <h2 className="text-[#072000ff] text-base font-medium">{title}</h2>
         <p className="text-2xl font-bold mt-2">{value}</p>
       </div>
     );
