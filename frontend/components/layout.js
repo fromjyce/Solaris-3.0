1ae1cc2eb82e57703570446d81c73ef9b4391f98
Jayashre <jaya2004kra@gmail.com>
2024-12-17 23:09:24 +0530

    modified layout

--- frontend/components/layout.js
+++ frontend/components/layout.js
@@ -4,7 +4,7 @@ const Layout = ({ children }) => {
   return (
     <div className="flex">
       <Sidebar />
-      <main className="flex-grow p-6 bg-gray-100 min-h-screen overflow-y-auto ml-64">
+      <main className="flex-grow bg-gray-100 min-h-screen overflow-y-auto">
         {children}
       </main>
     </div>
