3d3eebfec52528cf4b4e3bb5ada47f178d8d5b2f
Jayashre <jaya2004kra@gmail.com>
2024-12-18 01:36:12 +0530

    added settings page

---
+++ frontend/pages/settings.js
@@ -0,0 +1,28 @@
+import ProfileSettings from '@/components/settings/ProfileSettings';
+import WalletSettings from '@/components/settings/WalletSettings';
+import NotificationPreferences from '@/components/settings/NotificationPreferences';
+import LanguageAndThemeSettings from '@/components/settings/LanguageAndThemeSettings';
+import Layout from "@/components/layout";
+import Head from "next/head";
+
+export default function Settings() {
+  return (
+    <>
+    <Head>
+        <title>Solaris 2.0 | Settings</title>
+    </Head>
+    <div className="flex min-h-screen bg-[#cfcbbdff]">
+        <Layout />
+      <div className="container mx-auto p-8">
+      <h1 className="text-4xl font-bold mb-6 kumbhSans text-[#072000ff]">Settings</h1>
+      <main className="mx-auto space-y-8">
+        <ProfileSettings />
+        <WalletSettings />
+        <NotificationPreferences />
+        <LanguageAndThemeSettings />
+        </main>
+      </div>
+    </div>
+    </>
+  );
+}
