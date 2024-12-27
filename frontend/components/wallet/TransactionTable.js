8a0cb5ce472a73eba644fa9b8f7638aae49acf0b
Jayashre <jaya2004kra@gmail.com>
2024-12-27 17:57:52 +0530

    modified

--- frontend/components/wallet/TransactionTable.js
+++ frontend/components/wallet/TransactionTable.js
@@ -1,18 +1,18 @@
 const TransactionTable = ({ transactions }) => {
     return (
-      <div className="bg-[#f4f4f4] shadow rounded p-4">
+      <div className="shadow rounded p-4">
         <table className="w-full table-auto border-collapse">
           <thead>
-            <tr className="bg-[#c3c3c3]">
-              <th className="p-3 text-left text-[#072000ff]">ID</th>
-              <th className="p-3 text-left text-[#072000ff]">Type</th>
-              <th className="p-3 text-left text-[#072000ff]">Amount</th>
-              <th className="p-3 text-left text-[#072000ff]">Date</th>
+            <tr className="bg-[#f4f4f4]">
+              <th className="p-3 text-left text-[#072000ff] gabarito">ID</th>
+              <th className="p-3 text-left text-[#072000ff] gabarito">Type</th>
+              <th className="p-3 text-left text-[#072000ff] gabarito">Amount</th>
+              <th className="p-3 text-left text-[#072000ff] gabarito">Date</th>
             </tr>
           </thead>
           <tbody>
             {transactions.map((tx) => (
-              <tr key={tx.id} className="border-b hover:bg-[#7a7a7a]">
+              <tr key={tx.id} className="border-b afacad hover:bg-gray-100">
                 <td className="p-3">{tx.id}</td>
                 <td className="p-3">{tx.type}</td>
                 <td className="p-3">{tx.amount} RECs</td>
