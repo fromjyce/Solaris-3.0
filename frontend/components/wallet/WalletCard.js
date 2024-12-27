bec80672a5bb75c1788b58e22f69dea9dddf578e
Jayashre <jaya2004kra@gmail.com>
2024-12-27 18:04:20 +0530

    added final code

--- frontend/components/wallet/WalletCard.js
+++ frontend/components/wallet/WalletCard.js
@@ -1,22 +1,27 @@
-const WalletCard = ({ address, balance }) => {
-    return (
-      <div className="bg-[#f4f4f4] shadow-md rounded p-6 mt-6 max-w-lg mx-auto">
-        <h2 className="text-xl font-semibold text-[#072000ff] mb-4">
-          Wallet Overview
-        </h2>
-        <div className="flex flex-col space-y-2">
-          <p>
-            <span className="font-medium text-[#072000ff]">Wallet Address: </span>
-            {address}
-          </p>
-          <p>
-            <span className="font-medium text-[#072000ff]">Token Balance (RECs): </span>
-            {balance} RECs
-          </p>
-        </div>
+const WalletCard = ({ address, balance, onDisconnect }) => {
+  return (
+    <div className="w-full bg-[#f4f4f4] shadow-md rounded p-6">
+      <h2 className="text-3xl gabarito font-semibold text-[#072000ff] mb-4">
+        Wallet Overview
+      </h2>
+      <div className="flex flex-col space-y-2">
+        <p className="afacad text-xl">
+          <span className="font-medium text-[#072000ff]">Wallet Address: </span>
+          {address}
+        </p>
+        <p className="afacad text-xl">
+          <span className="font-medium text-[#072000ff]">Token Balance (RECs): </span>
+          {balance} RECs
+        </p>
       </div>
-    );
-  };
-  
-  export default WalletCard;
-  
+      <button
+        onClick={onDisconnect}
+        className="mt-6 px-6 py-3 bg-[#d32f2f] gabarito text-white rounded hover:bg-[#9a0007] transition"
+      >
+        Disconnect Wallet
+      </button>
+    </div>
+  );
+};
+
+export default WalletCard;
